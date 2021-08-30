a2ce225f7f7d75813c86090b6200473e1abac6ec
Luis Alexandre Deschamps Brandao <techmago@ymail.com>
2021-08-29 23:13:57 -0300

    pt-BR

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module Namebook
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.available_locales = [:en, :"pt-BR"]
+    config.i18n.default_locale = :"pt-BR"
   end
 end
